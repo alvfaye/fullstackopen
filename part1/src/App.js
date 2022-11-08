585ad7c17b5d510a12e01d7b4ed2af019763b6ca
alvfaye <alvfaye@gmail.com>
2022-11-09 03:20:41 +0800

    part content

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,35 +1,42 @@
 import React from 'react';
-
-const Header = ({ course }) => {
-  console.log('course', course);
-  return <div>Header {course}</div>;
+const course = {
+  title: 'Half Stack Application Development',
+  content: [
+    {
+      topic: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      topic: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      topic: 'State of a component',
+      exercises: 14,
+    },
+  ],
 };
 
-const Content = (props) => {
-  <div>{props.content}</div>;
-};
-const Total = () => {
-  <div>Total</div>;
-};
-
-export default function App() {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-  const content = [part1, part2, part3];
-  const total = exercises1 + exercises2 + exercises3;
-
-  console.log(content, total, course);
-
+function Part({ id, topic, exercises }) {
   return (
     <div>
-      <Header course={course} />
-      <Content content={content} />
-      <Total total={total} />
+      {topic} {exercises}
     </div>
   );
 }
+
+function Content({ course }) {
+  return (
+    <>
+      {course.map((x) => (
+        <Part topic={x.topic} exercises={x.exercises} />
+      ))}
+    </>
+  );
+}
+
+function App() {
+  return <Content course={course.content} />;
+}
+
+export default App;
