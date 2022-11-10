e9ca48d789326101f784e0b730b84156724667c3
alvfaye <alvfaye@gmail.com>
2022-11-10 23:34:56 +0800

    add feedback component

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,11 +1,29 @@
-import {useState} from 'react'
-
+import { useState } from 'react';
+const Feedback = () => {
+  return (
+    <>
+      <Button text="good" />
+      <Button text="neutral" />
+      <Button text="bad" />
+    </>
+  );
+};
+const Statistics = () => {};
+const Button = (props) => {
+  <button onClick={props.onClick}>{props.text}</button>;
+};
 function App() {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  return <div></div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Feedback />
+      <Statistics />
+    </div>
+  );
 }
 
 export default App;
