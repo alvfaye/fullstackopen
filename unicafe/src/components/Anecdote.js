71639275ffda2014ea7dcdc8dce33c7b528f9098
alvfaye <alvfaye@gmail.com>
2022-11-11 23:07:16 +0800

    add anecdote component

---
+++ unicafe/src/components/Anecdote.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later.',
+  'Premature optimization is the root of all evil!',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.'
+]
+
+function Anecdote() {
+  const [selected, setSelected] = useState(0)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Anecdote
