9012f8bea15ddebbaf70d9397abb3e2f34cbfe1a
alvfaye <alvfaye@gmail.com>
2022-11-11 23:37:09 +0800

    update button onclick

--- unicafe/src/components/Anecdote.js
+++ unicafe/src/components/Anecdote.js
@@ -7,15 +7,26 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
 ];
+const points = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+};
 
 function Anecdote() {
   const [selected, setSelected] = useState(0);
+  const clickVote = () => {
+    const copy = { ...points };
+    copy[selected] += 1;
+  }
   return (
     <div className="mt-4">
       <h1 className="font-extrabold">Anecdote of the day</h1>
       <h2>{anecdotes[selected]}</h2>
       <hr className="mb-2" />
-      <Button text="vote" onClick={() => console.log('vote')} />
+      <Button text="vote" onClick={clickVote} />
       <Button
         text="next anecdote"
         onClick={() => console.log('next anecdote')}
