a503309ec30927af93f0df4ffc83fc47d978667d
alvfaye <alvfaye@gmail.com>
2022-11-14 21:26:20 +0800

    add data

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,8 +1,8 @@
+import courses from './data';
 
 function App() {
-  return (
-    <h1 className="text-yellow-700">Hello courseinfo</h1>
-  );
+  console.log(courses);
+  return <h1 className="text-yellow-700">Hello courseinfo</h1>;
 }
 
 export default App;
