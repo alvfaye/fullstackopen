4c0e58b28e5f25883778de4d882f08401768d7a8
alvfaye <alvfaye@gmail.com>
2022-11-14 23:59:33 +0800

    upate part

--- part2/courseinfo/src/components/Part.js
+++ part2/courseinfo/src/components/Part.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Part({part}) {
-  return <div className="font-medium text-sky-900">{part.name} {part.exercises}</div>;
+  return (
+    <div className="font-medium text-sky-900">
+      {part.name} <span className="text-yellow-400">{part.exercises}</span>
+    </div>
+  );
 }
 
 export default Part;
