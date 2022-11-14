a15841373af9b7e93864fbb9f3826327dfae06a7
alvfaye <alvfaye@gmail.com>
2022-11-15 00:09:02 +0800

    update courseinfo

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -4,12 +4,12 @@ import { Part } from '../components';
 function Content({ parts }) {
   console.log('parts-->>',parts)
   return (<>
-    <div>
+    <div className="ml-2 border-2 p-2 rounded-md shadow-lg w-64 border-solid">
       {parts.map((part) => (
         <Part part={part} />
       ))}
     </div>
-    <div>Total Exercises {parts.reduce((total, num) => {
+    <div className="ml-2.5 mb-5">Total Exercises {parts.reduce((total, num) => {
       return total + num.exercises;
     }, 0)}
     </div>
--- part2/courseinfo/src/components/Header.js
+++ part2/courseinfo/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header({ name }) {
   return (
     <div>
-      <h1 className="font-extrabold">{name}</h1>
+      <h1 className="ml-1 mb-1 font-extrabold">{name}</h1>
     </div>
   );
 }
