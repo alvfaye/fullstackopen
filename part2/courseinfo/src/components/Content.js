85de88b8a7e3e31c4090421b2d6ccdc91cf270a2
alvfaye <alvfaye@gmail.com>
2022-11-14 23:49:37 +0800

    upate part

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -5,12 +5,18 @@ import { Part } from '../components';
 //   return <div>part</div>
 // }
 function Content({ parts }) {
-  return (
+  console.log('parts-->>',parts)
+  return (<>
     <div>
       {parts.map((part) => (
         <Part part={part} />
       ))}
     </div>
+    <div>Total Exercises {parts.reduce((total, num) => {
+      return total + num.exercises;
+    }, 0)}
+    </div>
+    </>
   );
 }
 
--- part2/courseinfo/src/components/Part.js
+++ part2/courseinfo/src/components/Part.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Part({part}) {
-  return <div className="font-medium text-sky-900">Part</div>;
+  return <div className="font-medium text-sky-900">{part.name} {part.exercises}</div>;
 }
 
 export default Part;
