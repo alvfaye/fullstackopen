35531c52ee3fc464560ad6ff0160400f1268581e
alvfaye <alvfaye@gmail.com>
2022-11-15 16:21:00 +0800

    restore code

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header, Content } from '../components';
 
-const Course = ({course }) => {
+const Course = ({ course }) => {
   return (
     <div>
       <Header name={course.name} />
