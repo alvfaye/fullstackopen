95f6b9f655ecc7c32e92239d7aaaa8db47a6823f
alvfaye <alvfaye@gmail.com>
2022-11-18 17:07:39 +0800

    add addl components

---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Filter(props) {
+  return (
+    <div>
+      Filter
+    </div>
+  )
+}
+
+export default Filter
---
+++ part2/phonebook/src/components/PersonForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PersonForm(props) {
+  return (
+    <div>
+      Person Form
+    </div>
+  )
+}
+
+export default PersonForm
