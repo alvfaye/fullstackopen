e85d9b45cb9bb443bf8ca41440392dbd299f93fb
alvfaye <alvfaye@gmail.com>
2022-11-18 17:03:11 +0800

    update structure of app

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 
 const App = () => {
- const [persons, setPersons] = useState([
-   { name: 'Arto Hellas', number: '040-123456', id: 1 },
-   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
- ]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
-
+  const [filter, setFilter] = useState('');
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+
+      <Filter filter={filter} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={persons} />
     </div>
   );
 };
