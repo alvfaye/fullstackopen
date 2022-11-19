8a20591f6f364d99113c77f1f89160649ecba685
alvfaye <alvfaye@gmail.com>
2022-11-19 08:31:08 +0800

    add note state

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 function App() {
+  const [note, setNote] = useState('a new note');
+
   return (
     <div className="w-[40%] p-3 font-semibold text-blue-900 border-2 shadow-md border-slate-500">
-      Notes
+      {note}
     </div>
   );
 }
