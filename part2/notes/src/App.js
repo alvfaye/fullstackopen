ee9ffbf5a386bb5bced4a000fc90bbc62a5a8094
alvfaye <alvfaye@gmail.com>
2022-11-19 22:23:44 +0800

    add handle submit

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -11,7 +11,9 @@ function App() {
         {note}
       </div>
       <form onSubmit={handleSubmit}>
+        <label for="note" className="text-sky-800 font-extrabold m-4">Enter new note</label>
         <input
+          name="note"
           value={note}
           type="text"
           onChange={(e) => setNote(e.target.value)}
