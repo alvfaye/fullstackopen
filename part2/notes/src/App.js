b39e7f9922e7326709cce34754423ec8eab43036
alvfaye <alvfaye@gmail.com>
2022-11-19 21:35:44 +0800

    add form to capture new note

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,22 @@
 import { useState } from 'react';
+
 function App() {
   const [note, setNote] = useState('a new note');
-
+  const handleSubmit = () => {
+    return <h3>handle submit</h3>;
+  };
   return (
-    <div className="w-[40%] p-3 font-semibold text-blue-900 border-2 shadow-md border-slate-500">
-      {note}
+    <div>
+      <div className="w-[40%] p-3 font-semibold text-blue-900 border-2 shadow-md border-slate-500">
+        {note}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={note}
+          type="text"
+          onChange={(e) => setNote(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
