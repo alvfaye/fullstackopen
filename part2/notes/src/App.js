8a9fb6c68877e7c99b3853f5e5fefbba02f6c744
alvfaye <alvfaye@gmail.com>
2022-11-19 23:18:17 +0800

    update display note form

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -12,16 +12,21 @@ function App() {
         {note}
       </div>
       <form onSubmit={handleSubmit}>
-        <label for="note" className="text-sky-800 font-extrabold m-4">
+        <label className="text-sky-800 font-extrabold m-4">
           Enter new note
+          <input
+            name="note"
+            value={note}
+            type="text"
+            onChange={(e) => setNote(e.target.value)}
+          />
         </label>
-        <input
-          name="note"
-          value={note}
-          type="text"
-          onChange={(e) => setNote(e.target.value)}
-        />
-        <button type="submit">submit</button>
+        <button
+          type="submit"
+          className="rounded-xl outline outline-red-200 shadow-red-400 shadow-2xl px-2 py-1 m-3"
+        >
+          submit
+        </button>
       </form>
     </div>
   );
