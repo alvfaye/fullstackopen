37f0da54696ed6e61201cac801a2540e3158e1ce
alvfaye <alvfaye@gmail.com>
2022-11-20 20:06:59 +0800

    update note app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -3,9 +3,12 @@ import { useState } from 'react';
 function App() {
   const [note, setNote] = useState('a new note');
   const handleSubmit = (event) => {
-    // return <h3>handle submit</h3>;
     event.preventDefault();
-    console.log('button clicked', event.target);
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setNote(value);
   };
 
   return (
@@ -13,19 +16,15 @@ function App() {
       <div className="w-[40%] p-3 font-semibold text-blue-900 border-2 shadow-md border-slate-500">
         {note}
       </div>
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-      >
+      {/* <form onSubmit={handleSubmit}> */}
+      <div>
         <label className="text-sky-800 font-extrabold mt-8 mr-5">
           Enter new note
           <input
-            className="from-neutral-900 font-normal mt-4 ml-4"
-            name="notefield"
-            value={note}
+            className="from-neutral-900 font-normal mt-4 ml-4 border border-violet-600"
+            name="note"
             type="text"
-            onChange={(e) => setNote(e.target.value)}
+            onChange={handleChange}
           />
         </label>
         <input
@@ -33,7 +32,8 @@ function App() {
           className="rounded-xl outline outline-red-200 shadow-red-400 shadow-2xl px-2 py-1 m-3"
           value="submit"
         />
-      </form>
+        {/* </form> */}
+      </div>
     </div>
   );
 }
