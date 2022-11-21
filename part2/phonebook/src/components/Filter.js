3d050fbdb20d078e830b56c1f8c6386f5e858547
alvfaye <alvfaye@gmail.com>
2022-11-21 16:57:00 +0800

    add style note form

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+const boxStyle = 'm-6 border shadow-md rounded-md w-[50%] p-4 leading-10';
 function Filter(props) {
   return (
     <div>
       <form>
-        <div>
-          <input type="text" name="title" placeholder="Title" />
-        </div>
-        <div>
-          <input type="text" name="text" placeholder="Take a note..." />
+        <div className={boxStyle}>
+          <div>
+            <input type="text" name="title" placeholder="Title" />
+          </div>
+          <div>
+            <input type="text" name="text" placeholder="Take a note..." />
+          </div>
         </div>
       </form>
     </div>
