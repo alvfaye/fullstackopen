3b83a580cbb7ef9bc9cdb4e73d27183a97cd5eec
alvfaye <alvfaye@gmail.com>
2022-11-21 17:28:00 +0800

    update phonebook table

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -11,13 +11,17 @@ const Person = ({ name, number }) => {
 function Persons({ persons }) {
   return (
     <table className="m-6 shadow-lg bg-white text-sm leading-none">
-      <tr>
-        <td className="bg-blue-100 border text-left px-8 py-4">Name</td>
-        <td className="bg-blue-100 border text-left px-8 py-4">Number</td>
-      </tr>
-      {persons.map((person) => (
-        <Person name={person.name} number={person.number} />
-      ))}
+      <thead>
+        <tr>
+          <td className="bg-blue-100 border text-left px-8 py-4">Name</td>
+          <td className="bg-blue-100 border text-left px-8 py-4">Number</td>
+        </tr>
+      </thead>
+      <tbody>
+        {persons.map((person) => (
+          <Person key={person.id} name={person.name} number={person.number} />
+        ))}
+      </tbody>
     </table>
   );
 }
