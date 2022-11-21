cb01c5f94019cbfff7fd4b8cfd891fc0a1e44b54
alvfaye <alvfaye@gmail.com>
2022-11-21 15:15:24 +0800

    update phonebook app

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 const Person = ({ name, number }) => {
   return (
-    <h3>
-      {name} {number}
-    </h3>
+    <tr>
+      <td className="border px-8 py-4">{name}</td>
+      <td className="border px-8 py-4">{number}</td>
+    </tr>
   );
 };
 function Persons({ persons }) {
   return (
-    <div>
+    <table className="m-6 shadow-lg bg-white text-sm leading-none">
+      <tr>
+        <td className="bg-blue-100 border text-left px-8 py-4">Name</td>
+        <td className="bg-blue-100 border text-left px-8 py-4">Number</td>
+      </tr>
       {persons.map((person) => (
-        <Person name={persons.name} number={persons.number} />
+        <Person name={person.name} number={person.number} />
       ))}
-    </div>
+    </table>
   );
 }
 
