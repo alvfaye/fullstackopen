e88a702f3e081adb96c6c244265727c0cd090e35
alvfaye <alvfaye@gmail.com>
2022-11-25 21:49:49 +0800

    add env notes

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios'
+const promise = axios.get('http://localhost:3001/notes')
+console.log(promise)
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
