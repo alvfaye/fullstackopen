e72fc3e831a5c706c1721823021fbacf95f866fe
alvfaye <alvfaye@gmail.com>
2022-11-28 16:35:24 +0800

    update search countries

--- part2/countries/src/components/Search.jsx
+++ part2/countries/src/components/Search.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const url = 'https://restcountries.com/v3.1/name/';
 
 function Search() {
+  const [name, setName] = useState('');
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}`)
+      .then((response) => {
+        //console.log(response.data);
+        setCountries(response.data);
+        console.log(countries);
+      });
+  }, []);
+
   return (
     <div>
-      <div className="text-4xl font-serif text-amber-800">find countries</div>
+      <div className="text-4xl font-serif text-amber-800">
+        <label>
+          find countries
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <h1>Countries</h1>
+      </div>
     </div>
   );
 }
