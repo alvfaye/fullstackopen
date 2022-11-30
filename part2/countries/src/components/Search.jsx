b87eb1407bd8bb21c4990fcfef97455cb06d02ea
alvfaye <alvfaye@gmail.com>
2022-11-30 23:03:33 +0800

    clean up code search

--- part2/countries/src/components/Search.jsx
+++ part2/countries/src/components/Search.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// const url = 'https://restcountries.com/v3.1/name/';
 
 function Search() {
   const [name, setName] = useState('');
   const [countries, setCountries] = useState([]);
   const [tmpName, settmpName] = useState('');
-  // setName('south sudan');
   useEffect(() => {
     const fetchData = async () => {
       const data = await axios.get(
@@ -15,12 +13,6 @@ function Search() {
       console.log('inside fetchdata', data);
       return data;
     };
-    // axios
-    //   .get(`https://restcountries.com/v3.1/name/${name}`, {
-    //     params: {
-    //       per_page: 10,
-    //     },
-    //   })
     fetchData()
       .then((response) => {
         console.log('inside response', name, response.data);
@@ -63,17 +55,12 @@ function Search() {
         <button
           className="border-2 border-green-200 text-xl px-2 py-1 mx-3 bg-green-200 rounded-2xl shadow-lg"
           type="button"
-          // onClick={(e) => {
-          //   console.log('country', country);
-          // }}
           onClick={modal}
-          // data-bs-toggle="modal"
-          // data-bs-target="#exampleModal"
         >
           Show
         </button>
 
-        {/* Modal */}
+       
       </div>
     );
   };
@@ -169,7 +156,6 @@ function Search() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          {/* <button onClick={(e) => setName(tmpName)}>submit</button> */}
         </label>
         <h1>Countries</h1>
         {countries.map((country, index) => (
