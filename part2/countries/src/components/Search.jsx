c44f2a08017afc62ee43d54e043d9f67d7b494be
alvfaye <alvfaye@gmail.com>
2022-12-01 17:21:54 +0800

    add state selected country

--- part2/countries/src/components/Search.jsx
+++ part2/countries/src/components/Search.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Search() {
-  const [name, setName] = useState('');
+  const [name, setName] = useState('sudan');
   const [countries, setCountries] = useState([]);
   const [tmpName, settmpName] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await axios.get(
@@ -19,7 +20,7 @@ function Search() {
         setCountries(response.data);
       })
       .catch(console.error);
-  }, [name]);
+  }, []);
 
   const Details = ({ country }) => {
     const languages = Object.values(country.languages);
@@ -47,93 +48,93 @@ function Search() {
         <button
           className="border-2 border-green-200 text-xl px-2 py-1 mx-3 bg-green-200 rounded-2xl shadow-lg"
           type="button"
-          onClick={() => <Modal country={country} />}
+          onClick={() => setSelectedCountry(country)}
         >
           Show
         </button>
-        <Details country={country} />
+        
       </div>
     );
   };
 
-  function Modal({ country }) {
-    console.log('inside modal---country');
-    return (
-      <div
-        className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog relative w-auto pointer-events-none">
-          <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-            <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-              <h5
-                className="text-xl font-medium leading-normal text-gray-800"
-                id="exampleModalLabel"
-              >
-                {country.name.common}
-              </h5>
-              <button
-                type="button"
-                className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body relative p-4">{country.common}</div>
-            <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-              <button
-                type="button"
-                className="px-6
-          py-2.5
-          bg-purple-600
-          text-white
-          font-medium
-          text-xs
-          leading-tight
-          uppercase
-          rounded
-          shadow-md
-          hover:bg-purple-700 hover:shadow-lg
-          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-purple-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                className="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out
-      ml-1"
-              >
-                Save changes
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // function Modal({ country }) {
+  //   console.log('inside modal---country');
+  //   return (
+  //     <div
+  //       className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+  //       id="exampleModal"
+  //       tabindex="-1"
+  //       aria-labelledby="exampleModalLabel"
+  //       aria-hidden="true"
+  //     >
+  //       <div className="modal-dialog relative w-auto pointer-events-none">
+  //         <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+  //           <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+  //             <h5
+  //               className="text-xl font-medium leading-normal text-gray-800"
+  //               id="exampleModalLabel"
+  //             >
+  //               {country.name.common}
+  //             </h5>
+  //             <button
+  //               type="button"
+  //               className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+  //               data-bs-dismiss="modal"
+  //               aria-label="Close"
+  //             ></button>
+  //           </div>
+  //           <div className="modal-body relative p-4">{country.common}</div>
+  //           <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+  //             <button
+  //               type="button"
+  //               className="px-6
+  //         py-2.5
+  //         bg-purple-600
+  //         text-white
+  //         font-medium
+  //         text-xs
+  //         leading-tight
+  //         uppercase
+  //         rounded
+  //         shadow-md
+  //         hover:bg-purple-700 hover:shadow-lg
+  //         focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
+  //         active:bg-purple-800 active:shadow-lg
+  //         transition
+  //         duration-150
+  //         ease-in-out"
+  //               data-bs-dismiss="modal"
+  //             >
+  //               Close
+  //             </button>
+  //             <button
+  //               type="button"
+  //               className="px-6
+  //     py-2.5
+  //     bg-blue-600
+  //     text-white
+  //     font-medium
+  //     text-xs
+  //     leading-tight
+  //     uppercase
+  //     rounded
+  //     shadow-md
+  //     hover:bg-blue-700 hover:shadow-lg
+  //     focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+  //     active:bg-blue-800 active:shadow-lg
+  //     transition
+  //     duration-150
+  //     ease-in-out
+  //     ml-1"
+  //             >
+  //               Save changes
+  //             </button>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div>
@@ -152,6 +153,8 @@ function Search() {
         {countries.map((country, index) => (
           <Country key={index} country={country} />
         ))}
+
+        <Details details={selectedCountry} />
       </div>
     </div>
   );
