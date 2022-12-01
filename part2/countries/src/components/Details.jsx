a7cd75d9bdb792a36181485028c74b45a50b4f4b
alvfaye <alvfaye@gmail.com>
2022-12-01 18:19:22 +0800

    update details add flag img

--- part2/countries/src/components/Details.jsx
+++ part2/countries/src/components/Details.jsx
@@ -7,14 +7,17 @@ function Details({ country }) {
       //className="fixed inset-0 bg-gray-300 bg-opacity-50 overflow-y-auto h-full w-full"
       id="my-modal"
     >
+      {/* main details */}
       <h1>{country.name.common}</h1>
       <h2>capital {country.capital[0]}</h2>
       <h2>area {country.area}</h2>
+      {/* list languages */}
       <h3>languages:</h3>
-
       {languages.map((language, id) => (
         <h2 key={id}>{language}</h2>
       ))}
+      {/* display flag */}
+      <img src={country.flags.svg} />
     </div>
   );
 }
