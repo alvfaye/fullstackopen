74a166ca0d4cc93dfb6ea77bc917af31b2602f81
alvfaye <alvfaye@gmail.com>
2022-12-02 23:26:57 +0800

    update weather fetchdata

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -3,22 +3,32 @@ import axios from 'axios';
 import React, { useState } from 'react';
 const APIkey = 'b76dcd7e89746c4fd4baa3c11b0b0553';
 //process.env.OpenWeatherAPIkey;
-const fetchData = async (city) => {
+const fetchData = async (city, setWeatherData) => {
   console.log('APIkey', APIkey);
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
   console.log('api url', url);
   const data = await axios.get(url);
-  return data;
+  //data.then((response) => {
+  //setWeatherData(response.data);
+  console.log('data assigned to setWeatherDAta', data.data);
+  //});
+  return data.data;
 };
 
 function Weather({ city }) {
   const [weatherData, setWeatherData] = useState({});
   // const city = 'Manila,PH';
-  fetchData(city).then((response) => {
-    setWeatherData(response.data);
-    //console.log(weatherData);
-  });
-  return <div>{weatherData.weather.description}</div>;
+  setWeatherData(fetchData(city));
+  //.then((response) => {
+  //setWeatherData(response.data);
+  console.log('WEATHER DATA', weatherData);
+  //});
+
+  return (
+    <div>
+      {Object.keys(weatherData).length && weatherData.weather[0].description}
+    </div>
+  );
 }
 
 export default Weather;
