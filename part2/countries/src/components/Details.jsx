bbf7272677abdc97557e75a22270f94da29bb22d
alvfaye <alvfaye@gmail.com>
2022-12-02 16:30:26 +0800

    update details

--- part2/countries/src/components/Details.jsx
+++ part2/countries/src/components/Details.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 
 function Details({ country }) {
-  let languages = Object.values(country.languages);
-  // let languages = {
-  //   ara: 'Arabic',
-  //   eng: 'English',
-  // };
+
+  let languages = ['Arabic', 'English'];
+  // let languages = Object.values(country?.languages);
+    console.log('languages', languages);
   return (
-    <div
-      //className="fixed inset-0 bg-gray-300 bg-opacity-50 overflow-y-auto h-full w-full"
-      id="my-modal"
-    >
-      {/* main details */}
-      <h1>{country.name.common}</h1>
-      <h2>capital {country.capital[0]}</h2>
-      <h2>area {country.area}</h2>
-      {/* list languages */}
-      <h3>languages:</h3>
-      {languages.map((language, id) => (
-        <h2 key={id}>{language}</h2>
-      ))}
-      {/* display flag */}
-      <img src={country.flags.svg} />
-    </div>
+    Object.keys(country).length && (
+      <div
+        //className="fixed inset-0 bg-gray-300 bg-opacity-50 overflow-y-auto h-full w-full"
+        id="my-modal"
+      >
+        {/* main details */}
+        <h1>{country.name.common}</h1>
+        <h2>capital {country.capital[0]}</h2>
+        <h2>area {country.area}</h2>
+        {/* list languages */}
+        <h3>languages:</h3>
+        {
+          
+          languages.map((language, id) => (
+          <h2 key={id}>{language}</h2>
+          ))
+        }
+        {/* display flag */}
+        <img src={country.flags.svg} />
+      </div>
+    )
   );
 }
 
