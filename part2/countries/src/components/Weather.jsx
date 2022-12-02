f9e827dac36ac45669dbae9371004640bce533cb
alvfaye <alvfaye@gmail.com>
2022-12-02 22:16:59 +0800

    update weather component

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -1,7 +1,19 @@
+import axios from 'axios';
 import React from 'react';
+const APIkey = process.env.OpenWeatherAPIkey;
+const fetchData = async (city) => {
+  const data = await axios.get(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`
+  );
+  return data;
+};
 
-function Weather({ country }) {
-  return <div>Weather</div>;
+function Weather({ location }) {
+  const [weatherData, setWeatherData] = useState({});
+  fetchData(location).then((response) => {
+    setWeatherData(response.data);
+  });
+  return <div>{weatherData}</div>;
 }
 
 export default Weather;
