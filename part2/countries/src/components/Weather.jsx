82ef8b7a50b8a67ef7fe9fd15d29d3db14d69e5c
alvfaye <alvfaye@gmail.com>
2022-12-02 22:26:38 +0800

    update weather

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const APIkey = process.env.OpenWeatherAPIkey;
 const fetchData = async (city) => {
   const data = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${APIkey}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
   );
   return data;
 };
 
 function Weather({ location }) {
   const [weatherData, setWeatherData] = useState({});
+  const location = 'Manila,PH';
   fetchData(location).then((response) => {
     setWeatherData(response.data);
   });
