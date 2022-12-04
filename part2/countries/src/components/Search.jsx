8632f3b61add6b94419e792ed5a5ba612bee683d
alvfaye <alvfaye@gmail.com>
2022-12-04 21:24:01 +0800

    debug weather comp

--- part2/countries/src/components/Search.jsx
+++ part2/countries/src/components/Search.jsx
@@ -82,8 +82,9 @@ function Search() {
           'selectedCountry exist',
           Object.keys(selectedCountry).length
         )}
+        {/* {console.log('selectedCountry', selectedCountry.name.common)} */}
         <Details country={selectedCountry} />
-        <h3>Weather in {selectedCountry.capital[0]}</h3>
+        <h3>Weather in { selectedCountry.capital[0] }</h3>
         <Weather city={selectedCountry.capital[0]} />
       </div>
     </div>
