e32bf3dafb065c96316404f0ebdcc82132631db8
alvfaye <alvfaye@gmail.com>
2022-12-06 20:46:51 +0800

    update weather function

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-const APIkey = 'b76dcd7e89746c4fd4baa3c11b0b0553';
-//process.env.OpenWeatherAPIkey;
+// const APIkey = 'b76dcd7e89746c4fd4baa3c11b0b0553';
+const APIkey = process.env.OpenWeatherAPIkey;
+const weatherObj = {};
 
-const Test = (city, setWeatherData) => {
+const Test = (city) => {
   const fetchData = async (city) => {
     console.log('APIkey', APIkey);
     //const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
     const url = `http://localhost:3004/weather`;
-
     const { data } = await axios.get(url);
 
     //console.log('data assigned to setWeatherDAta', JSON.stringify(data));
@@ -20,12 +20,13 @@ const Test = (city, setWeatherData) => {
     //return data;
     return weathr[0];
   };
+
   fetchData(city)
     .then((response) => {
       console.log('inside response', city, JSON.stringify(response));
 
       const { weather, main, wind } = response;
-      const weatherObj = {
+      weatherObj = {
         desc: weather[0].description,
         icon: weather[0].icon,
         temp: main.temp,
@@ -38,21 +39,21 @@ const Test = (city, setWeatherData) => {
       <img src="" />
       `;
 
-      setWeatherData(weatherObj);
+      //setWeatherData(weatherObj);
       console.log('desc', desc, JSON.stringify(weatherObj));
     })
     .catch(console.error);
 };
 
 const Weather = ({ city }) => {
-  const [weather, setWeatherData] = useState({});
-  Test(city, setWeatherData);
+  //const [weather, setWeatherData] = useState({});
+  Test(city);
   //weather.icon = '10d';
-  console.log('weather data', city, JSON.stringify(weather));
+  console.log('weather data', city, JSON.stringify(weatherObj));
   return (
     <>
-      <div>{weather.desc}</div>
-      <img src={`/images/${weather.icon}@2x.png`} />
+      <div>{weatherObj.desc}</div>
+      <img src={`/images/${weatherObj.icon}@2x.png`} />
     </>
   );
 };
