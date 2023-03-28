3ea7b5cb780a00728434c1fe63e3229c9364df06
alvfaye <alvfaye@gmail.com>
2023-03-28 22:57:54 +0800

    update part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,31 @@ import { useState, useEffect } from 'react';
 import { Filter, Persons, PersonForm } from './components';
 import axios from 'axios';
 
+const url = 'http://localhost:3004/persons';
+// async function getPersons(url) {
+//   try {
+//     const response = await axios.get(url)
+//     console.log(response.data)
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// const items = getData(url)
+// console.log(items)
+
+// async function getData(url) {
+//   const response = await fetch(url, {
+//     method: 'GET',
+//     mode: 'cors',
+//     headers: {
+//       'X-API-Key': 'Testing',
+//       'Content-Type': 'application/json'
+//     }
+//   })
+//   console.log(response)
+// }
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -9,6 +34,8 @@ const App = () => {
   //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
   //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   // ]);
+
+  //console.log(getData(url))
 
   const [filteredList, setFilteredList] = useState(persons);
   const addNewContact = (name, number) => {
@@ -36,11 +63,13 @@ const App = () => {
   };
 
   useEffect(() => {
-    axios.get('http://localhost:3004/persons').then((response) => {
-      console.log(response.data);
-      setPersons(response.data);
-      setFilteredList(response.data);
-    });
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        setPersons(response.data);
+        setFilteredList(response.data);
+    },[]);
     // effect
     // return () => {
     //   cleanup
